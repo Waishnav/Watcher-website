d060961fe7a8029a18995166fafbe67dd5b57383
Vansh Mittal <103214265+mittalvansh@users.noreply.github.com>
2022-10-15 00:13:07 +0530

    Added Header Component! (#3)
    * Added Header Component!
    
    * Changes Done!
    
    * Update Header.css
    
    * Update index.html

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import main_img from '../.././assets/logo.svg';
+import up_arrow from '../.././assets/up-arrow.svg';
+import git_img from '../.././assets/github.svg';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="Container">
+            <div className="Container_1">
+                <img src={main_img} alt="Watcher-Logo" />
+                <p>
+                    Watcher
+                    <span className="borderBottom"></span>
+                    <img src={up_arrow} alt="" />
+                </p>
+            </div>
+
+            <a href="https://github.com/Waishnav/Watcher">
+                <div className="Container_2">
+                    <img src={git_img} alt="" />
+                    <div className="StarUs">STAR US</div>
+                    <div className="vertical"></div>
+                    <div className="Likes">107</div>
+                </div>
+            </a>
+        </div >
+    );
+}
+
+export default Header;
