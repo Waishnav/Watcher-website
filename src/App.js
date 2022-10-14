a310435173c485b0827491bb7522868730f9ebdc
Vansh Mittal <mittalvansh69@gmail.com>
2022-10-14 14:20:31 +0530

    Added Header Component!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
