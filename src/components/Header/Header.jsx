fa4078cb1b94326672a372bbe35969553c8a2296
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2022-10-16 01:59:33 +0530

    feat: 🎸 added stargazer count

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import main_img from '../.././assets/logo.svg';
 import up_arrow from '../.././assets/up-arrow.svg';
 import git_img from '../.././assets/github.svg';
 import './Header.css';
 
 function Header() {
+    const [stars, setStars] = useState(100);
+    useEffect(() => {
+        async function getStars() {
+            const res = await fetch("https://api.github.com/repos/waishnav/watcher");
+            const jsonRes = await res.json();
+            setStars(jsonRes.stargazers_count);
+        }
+        getStars();
+    }, [stars]);
+
     return (
         <div className="Container">
             <div className="Container_1">
@@ -21,7 +31,7 @@ function Header() {
                     <img src={git_img} alt="" />
                     <div className="StarUs">STAR US</div>
                     <div className="vertical"></div>
-                    <div className="Likes">107</div>
+                    <div className="Likes">{stars}</div>
                 </div>
             </a>
         </div >
