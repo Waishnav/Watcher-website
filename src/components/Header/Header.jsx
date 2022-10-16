2134bcfcbf7887ac0dbb266573104af8332b4a78
Vansh Mittal <mittalvansh69@gmail.com>
2022-10-16 23:33:17 +0530

    Added Eye Animation Feature!

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,24 @@
-import React from "react";
-import main_img from '../.././assets/logo.svg';
+import React, { useEffect, useState } from "react";
 import up_arrow from '../.././assets/up-arrow.svg';
 import git_img from '../.././assets/github.svg';
 import './Header.css';
+import Eye from "../Eye/Eye";
 
 function Header() {
+    const [stars, setStars] = useState(100);
+    useEffect(() => {
+        async function getStars() {
+            const res = await fetch("https://api.github.com/repos/waishnav/watcher");
+            const jsonRes = await res.json();
+            setStars(jsonRes.stargazers_count);
+        }
+        getStars();
+    }, [stars]);
+
     return (
         <div className="Container">
             <div className="Container_1">
-                <img src={main_img} alt="Watcher-Logo" />
+                <Eye className="logo" />
                 <p>
                     Watcher
                     <span className="borderBottom"></span>
@@ -21,7 +31,7 @@ function Header() {
                     <img src={git_img} alt="" />
                     <div className="StarUs">STAR US</div>
                     <div className="vertical"></div>
-                    <div className="Likes">107</div>
+                    <div className="Likes">{stars}</div>
                 </div>
             </a>
         </div >
