3a5d776095d0b2e3c3455cb3ea95084fe1cdd772
Prajjwal kapoor <68912239+prajjwalkapoor@users.noreply.github.com>
2022-10-24 16:55:35 +0530

    refactor: css modules (#21)
    Co-authored-by: Prajjwal Kapoor <prajjwal.kapoor.mail@gmail@con>

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,41 @@
 import React, { useEffect, useState } from "react";
-import up_arrow from '../.././assets/up-arrow.svg';
-import git_img from '../.././assets/github.svg';
-import './Header.css';
+import up_arrow from "../.././assets/up-arrow.svg";
+import git_img from "../.././assets/github.svg";
+import styles from "./Header.module.css";
 import Eye from "../Eye/Eye";
 
 function Header() {
-    const [stars, setStars] = useState(100);
-    useEffect(() => {
-        async function getStars() {
-            const res = await fetch("https://api.github.com/repos/waishnav/watcher");
-            const jsonRes = await res.json();
-            setStars(jsonRes.stargazers_count);
-        }
-        getStars();
-    }, [stars]);
+  const [stars, setStars] = useState(100);
+  useEffect(() => {
+    async function getStars() {
+      const res = await fetch("https://api.github.com/repos/waishnav/watcher");
+      const jsonRes = await res.json();
+      setStars(jsonRes.stargazers_count);
+    }
+    getStars();
+  }, [stars]);
 
-    return (
-        <div className="Container">
-            <div className="Container_1">
-                <Eye className="logo" />
-                <p>
-                    Watcher
-                    <span className="borderBottom"></span>
-                    <img src={up_arrow} alt="" />
-                </p>
-            </div>
+  return (
+    <div className={styles.Container}>
+      <div className={styles.Container_1}>
+        <Eye className={styles.logo} />
+        <p>
+          Watcher
+          <span className={styles.borderBottom}></span>
+          <img src={up_arrow} alt='' />
+        </p>
+      </div>
 
-            <a href="https://github.com/Waishnav/Watcher">
-                <div className="Container_2">
-                    <img src={git_img} alt="" />
-                    <div className="StarUs">STAR US</div>
-                    <div className="vertical"></div>
-                    <div className="Likes">{stars}</div>
-                </div>
-            </a>
-        </div >
-    );
+      <a href='https://github.com/Waishnav/Watcher'>
+        <div className={styles.Container_2}>
+          <img src={git_img} alt='' />
+          <div className={styles.StarUs}>STAR US</div>
+          <div className={styles.vertical}></div>
+          <div className={styles.Likes}>{stars}</div>
+        </div>
+      </a>
+    </div>
+  );
 }
 
 export default Header;
